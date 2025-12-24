68a35844b9435c51b0d04af23d16a8794346aa46
DESKTOP-L3VIVLD\hy_glory <joywithjoy1203@gmail.com>
2025-12-24 12:29:18 +0900

    fear/기본 header, footer 구조 적용

---
+++ app/layout.tsx
@@ -0,0 +1,25 @@
+import Header from './Layout/Header';
+import Footer from './Layout/Footer';
+
+export const metadata = {
+  title: 'Gentle Viking',
+  description: 'Ai 포트폴리오 구성 및 자동매매 서비스',
+};
+
+export default function Layout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="ko">
+      <div className="min-h-screen flex flex-col">
+        <Header />
+        <main className="flex-1 bg-gray-100 px-lg py-lg">
+          {children}
+        </main>
+        <Footer />
+      </div>
+    </html>
+  );
+}
